673feaa3e312f30ce7e42d2e602b299c8b2bca97
blaise82 <nigorjeanluc@gmail.com>
2020-03-08 08:34:17 +0200

    Add API for all failling tests

--- server/index.js
+++ server/index.js
@@ -2,57 +2,107 @@ import express from 'express';
 import dotenv from 'dotenv';
 
 const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 const todos = [
   {
     id: 1,
     title: 'Some title One',
     completed: false,
-    priority: 3,
+    priority: 3
   },
   {
     id: 2,
     title: 'Some title Two',
     completed: false,
-    priority: 2,
+    priority: 2
   },
   {
     id: 3,
     title: 'Some title Three',
-    completed: 'false',
-    priority: 1,
-  },
+    completed: false,
+    priority: 1
+  }
 ];
 
 dotenv.config();
 
 
 app.get('/', (req, res) => {
-  res.send({ message: 'Welcome to the TDD Concept App' });
+  return res.send({ message: 'Welcome to the TDD Concept App' });
 });
 
-app.get('/todos', (req, res) => {
-    res.status(200).json({
+app.get('/tasks', (req, res) => {
+  return res.status(200).json({
+    status: 200,
+    message: 'Retrieved all tasks',
+    data: todos,
+  });
+});
+
+app.post('/tasks', (req, res) => {
+  const id = todos.length + 1;
+  todos.push({id, ...req.body});
+  const matchTodo = todos.find((todo) => todo.id === parseInt(id, 10));
+  return res.status(201).json({
+      status: 201,
+      message: 'Retrieved all tasks',
+      data: matchTodo
+  });
+});
+
+app.get('/tasks/:id', (req, res) => {
+  const matchTodo = todos.find((todo) => todo.id === parseInt(req.params.id, 10));
+  return res.status(200).json({
+    status: 200,
+    message: 'Task is fetched successfully',
+    data: matchTodo,
+  });
+});
+
+app.delete('/tasks/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  const todo = todos.find(todo => todo.id === id);
+  todos.splice(todos.indexOf(todo), 1);
+  return res.sendStatus(204);
+});
+
+app.patch('/tasks/:id', (req, res)=>{
+  const taskId = parseInt(req.params.id, 10);
+  const updatedTodo = req.body;
+
+  todos.map((todo, index) => {
+    if(todo.id === taskId) {
+      todo.id        = parseInt(updatedTodo.id, 10)    || todo.id;
+      todo.title     = updatedTodo.title || todo.title;
+      todo.completed = updatedTodo.completed || todo.completed;
+      todo.priority  = updatedTodo.priority || todo.priority;
+
+      return res.status(200).json({
         status: 200,
-        message: 'Retrieved all todos',
-        data: todos,
-    });
+        message: 'todo updated successfully',
+        data: todo
+      });
+    }
+  })
+
+  return res.status(404).json({
+    status: 404,
+    error: 'The given id is not found'
+  });
 });
-
-
-// app.get('todos/:id', (req, res) => {
-//     console.log(res.params);
-//     const matchTodo = todos.find((todo) => todo.id === parseInt(res.params.id, 10));
-//     res.status(200).json({
-//         status: 200,
-//         message: 'Fetched todo successful',
-//         data: matchTodo,
-//     });
-// });
 
 app.get('/*', (req, res) => {
-    res.sendStatus(404);
+  return res.status(404).json({
+    status: 404,
+    error: 'Not Found'
+  });
 });
 
 app.listen(process.env.PORT, () => {
     console.log(`App running on PORT ${process.env.PORT}`);
 });
+
+export { todos };
+export default app;
--- server/tests/todos.test.js
+++ server/tests/todos.test.js
@@ -1,71 +1,113 @@
-import app from '../index';
+import app, { todos } from '../../server';
 import chai from 'chai';
-import supertest from 'supertest';
+import chaiHttp from "chai-http";
 
-const expect = chai.expect;
+const {expect} = chai;
+chai.use(chaiHttp);
 
-describe('API Tests', function() {
-  var task = {
+describe('API Tests', () => {
+  let task = {
     title: 'integration test',
+    completed: false,
+    priority: 4
   };
 
-  describe('## Create task ', function() {
-    it('should create a task', function(done) {
-      request(app)
-        .post('/tasks')
+  describe('### Welcome message ', () => {
+    it('should welcome a user', (done) => {
+      chai.request(app)
+        .get('/')
+        .end((err, res) => {
+          expect(res.body.message).to.equal('Welcome to the TDD Concept App');
+          done();
+        });
+    });
+  });
+
+  describe('## Create task ', () => {
+    it('should create a task', (done) => {
+      chai.request(app)
+        .post("/tasks")
         .send(task)
-        .end(function(err, res) {
-          expect(res.status).to.equal(200);
-          expect(res.body.title).to.equal('integration test');
+        .end((err, res) => {
+          expect(res.status).to.equal(201);
+          expect(res.body.data.title).to.equal('integration test');
           task = res.body;
           done();
         });
     });
   });
 
-  describe('# Get all tasks', function() {
-    it('should get all tasks', function(done) {
-      request(app)
+  describe('# Get all tasks', () => {
+    it('should get all tasks', (done) => {
+      chai.request(app)
         .get('/tasks')
-        .end(function(err, res) {
+        .end((err, res) => {
           expect(res.status).to.equal(200);
-          expect(res.body).to.be.an('array');
+          expect(res.body).to.be.an('object');
+          expect(res.body.data).to.be.an('array');
+          expect(res.body.message).to.equal('Retrieved all tasks');
           done();
         });
     });
   });
-  describe('Get a task by id', function() {
-    it('should get a task', function(done) {
-      request(app)
-        .get('/tasks/' + task._id)
-        .end(function(err, res) {
+  describe('Get a task by id', () => {
+    it('should get a task', (done) => {
+      chai.request(app)
+        .get('/tasks/' + todos[0].id)
+        .end((err, res) => {
           expect(res.status).to.equal(200);
-          expect(res.body.title).to.equal('integration test');
+          expect(res.body.message).to.equal('Task is fetched successfully');
+          expect(res.body.data.title).to.equal('Some title One');
           done();
         });
     });
   });
 
-  describe('Update a task by id', function() {
-    it('should modify a task', function(done) {
+  describe('Update a task by id', () => {
+    it('should modify a task', (done) => {
       task.title = 'New Task'
-      request(app)
-        .put('/tasks/' + task._id)
+      chai.request(app)
+        .patch('/tasks/' +  todos[todos.length - 1].id)
         .send(task)
-        .end(function(err, res) {
-          expect(res.body.title).to.equal('New Task');
+        .end((err, res) => {
           expect(res.status).to.equal(200);
+          expect(res.body.data.title).to.equal('New Task');
           done();
         });
     });
   });
-  describe('Delete a task by id', function() {
-    it('should delete a task', function(done) {
-      request(app)
-        .delete('/tasks/' + task._id)
-        .end(function(err, res) {
-          expect(res.status).to.equal(200);
-          expect(res.body.message).to.equal('Task successfully deleted');
+
+  describe('Update a task by id with unkown id', () => {
+    it('should not modify a task', (done) => {
+      task.title = 'New Task'
+      chai.request(app)
+        .patch('/tasks/' +  todos.length + 100)
+        .send(task)
+        .end((err, res) => {
+          expect(res.body.error).to.equal('The given id is not found');
+          expect(res.status).to.equal(404);
+          done();
+        });
+    });
+  });
+
+  describe('Delete a task by id', () => {
+    it('should delete a task', (done) => {
+      chai.request(app)
+        .delete('/tasks/' + todos[todos.length - 1].id)
+        .end((err, res) => {
+          expect(res.status).to.equal(204);
+          done();
+        });
+    });
+  });
+  describe('Not found', () => {
+    it('should not allow unknown address', (done) => {
+      chai.request(app)
+        .get('/iwannathankmeforneverquitting')
+        .end((err, res) => {
+          expect(res.status).to.equal(404);
+          expect(res.body.error).to.equal('Not Found');
           done();
         });
     });
